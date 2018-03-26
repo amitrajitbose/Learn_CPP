880e547e21c63ba73e5ed32fe527c808f7b9ee37
amitrajitbose <amitrajitbose9@gmail.com>
2018-03-26 22:27:21 +0530

    Add Program 4

---
+++ Variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+	//<Type><Variable_Name>;
+	int a=15;
+	char b='B';
+	bool c=true;
+	float d=0.525;
+	long e=11124435;
+	long long f=353545518;
+	double g=0.002404;
+	std::string h="Jeet";
+	//std::string and std::vector are user defined variable datatypes
+
+	std::cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h;
+	std::cin.get();
+	return 0;
+}
