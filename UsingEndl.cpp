0906dce29d9f8ff149af11c9293bfc93d5457add
amitrajitbose <amitrajitbose9@gmail.com>
2018-03-26 22:26:37 +0530

    Add Program 2

---
+++ UsingEndl.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+int main()
+{
+    std::cout<<2018<<" is the present year."<<std::endl;
+    //endl adds a new line after the output
+    std::cout<<2017<<" is the past year."<<std::endl;
+    std::cout<<2019<<" is the upcoming year."<<std::endl<<std::endl;
+    std::cin.get(); //halts the screen (on my PC)
+    
+    //In the last line I added two 'endl' thus it gave me two new lines
+    //It is absolutely similar to the Enter key on keyboard
+    return 0;
+}
