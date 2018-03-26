839368b6aa4d3a2568a20f310271bb5b92612c80
amitrajitbose <amitrajitbose9@gmail.com>
2018-03-26 22:24:25 +0530

    Add Program 1

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    //This prints the message
+    std::cout<<"Hello World!";
+    //This halts the console and waits for user to press 'Enter'
+    std::cin.get();
+    return 0;
+}
