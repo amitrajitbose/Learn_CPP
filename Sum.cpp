436e376b23b874bfa9d987235c193d98d2ab6a17
amitrajitbose <amitrajitbose9@gmail.com>
2018-03-27 22:06:39 +0530

    Add program 7

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2,n3,n4,n5;
+    cout << "Enter 5 integers: ";
+    cin>>n1>>n2>>n3>>n4>>n5;
+    cout<<"SUM: "<<(n1+n2+n3+n4+n5)<<endl;
+    cout<<"AVERAGE: "<<(n1+n2+n3+n4+n5)/5.0;
+    cin.get();
+    cin.get();
+    return 0;
+}
