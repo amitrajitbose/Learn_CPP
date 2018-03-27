654039ffad74f3b2b6faf0b8092b43f4f30b28e3
amitrajitbose <amitrajitbose9@gmail.com>
2018-03-27 22:07:16 +0530

    Add program 9

---
+++ auto.cpp
@@ -0,0 +1,15 @@
+//Auto Keyword
+#include <iostream>
+using namespace std;
+int main()
+{
+    auto x=10.5; 
+    auto y=10;
+    
+    cout<<(x+y);
+    return 0;
+}
+/* This is a multi line comment
+
+Just like that*/
+
